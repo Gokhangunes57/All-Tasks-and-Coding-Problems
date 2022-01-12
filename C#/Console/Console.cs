3c812adddf66596270ad3dfba04bb16c0fe20db1
gokhan gunes <132ggokhann@gmail.com>
2022-01-12 21:45:20 +0300

    console c#

---
+++ C#/Console/Console.cs
@@ -0,0 +1,10 @@
+namespace Deneme
+{
+    class Patika
+    {
+        public static void Main(string[] args)
+        {
+            DateTime tarih = DateTime.Now;
+        }
+    }
+}
---
+++ C#/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 4;
+            Console.WriteLine(a);
+            DateTime saat = DateTime.Now;
+            String dakika=DateTime.Now.ToString();
+            Console.WriteLine(saat);
+        }
+    }
+}
