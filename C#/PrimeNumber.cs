1e93af37537ea0b7aba396c13a2739ace0569f1b
Gökhan güneş <132ggokhann@gmail.com>
2023-02-10 15:48:37 +0300

    Prime number console codes added

---
+++ C#/PrimeNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                int Counter = 0;
+                Console.WriteLine("Sayıyı Girin");
+                int Number = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= Number; i++)
+                {
+                    if (Number % i == 0)
+                    {
+                        Counter++;
+                    }
+                }
+                if (Counter == 2)
+                {
+                    Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayıdır.", Number);
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayı Değildir.", Number);
+                }
+                Console.ReadKey();
+            
+        }
+    }
+}
